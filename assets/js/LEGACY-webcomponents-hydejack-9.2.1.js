7b87272ff68232993ac98f33cf5d1488426c9f6c
Samuel M. <samesquit@gmail.com>
2025-07-14 10:47:58 +0200

    Update example content and metadata across multiple HTML files
    - Replaced placeholder text with a brief bio in example content files.
    - Updated INDEX_KEY timestamps in JavaScript search configurations for consistency.
    - Changed published time in meta tags to reflect the new date.
    - Updated feed.xml with the latest timestamp.
    - Modified index.html and posts/index.html to improve SEO metadata and titles.
    - Added Hydejack branding to the footer of various HTML files.

--- assets/js/LEGACY-webcomponents-hydejack-9.2.1.js
+++ assets/js/LEGACY-webcomponents-hydejack-9.2.1.js
@@ -1,7 +1,14 @@
-//   ____                             _   __  __                       _ _        
-//  / ___|  __ _ _ __ ___  _   _  ___| | |  \/  | ___  ___  __ _ _   _(_) |_ __ _ 
-//  \___ \ / _` | '_ ` _ \| | | |/ _ \ | | |\/| |/ _ \/ __|/ _` | | | | | __/ _` |
-//   ___) | (_| | | | | | | |_| |  __/ | | |  | |  __/\__ \ (_| | |_| | | || (_| |
-//  |____/ \__,_|_| |_| |_|\__,_|\___|_| |_|  |_|\___||___/\__, |\__,_|_|\__\__,_|
-//                                                            |_|               
+/*!
+ *  __  __                __                                     __
+ * /\ \/\ \              /\ \             __                    /\ \
+ * \ \ \_\ \   __  __    \_\ \      __   /\_\      __       ___ \ \ \/'\
+ *  \ \  _  \ /\ \/\ \   /'_` \   /'__`\ \/\ \   /'__`\    /'___\\ \ , <
+ *   \ \ \ \ \\ \ \_\ \ /\ \L\ \ /\  __/  \ \ \ /\ \L\.\_ /\ \__/ \ \ \\`\
+ *    \ \_\ \_\\/`____ \\ \___,_\\ \____\ _\ \ \\ \__/.\_\\ \____\ \ \_\ \_\
+ *     \/_/\/_/ `/___/> \\/__,_ / \/____//\ \_\ \\/__/\/_/ \/____/  \/_/\/_/
+ *                 /\___/                \ \____/
+ *                 \/__/                  \/___/
+ *
+ * Powered by Hydejack v9.2.1 <https://hydejack.com/>
+ */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[17],{337:function(e,n,t){"use strict";t.r(n);t(346),t(347),t(348),t(349);var o=window.customElements,d=!1,l=null;function a(){window.HTMLTemplateElement.bootstrap&&window.HTMLTemplateElement.bootstrap(window.document),l&&l(),d=!0,document.dispatchEvent(new CustomEvent("WebComponentsReady",{bubbles:!0}))}o.polyfillWrapFlushCallback&&o.polyfillWrapFlushCallback((function(e){l=e,d&&e()})),"complete"!==document.readyState?(window.addEventListener("load",a),window.addEventListener("DOMContentLoaded",(function(){window.removeEventListener("load",a),a()}))):a()}}]);
